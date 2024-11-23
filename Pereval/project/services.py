826d0f0087e5c7b20eaef806de47e980355f9d3f
vadimelisov <126413765+vadimelisov@users.noreply.github.com>
2024-11-23 21:51:08 +0300

    Delete Pereval directory

--- Pereval/project/services.py
+++
@@ -1,4 +0,0 @@
-def get_path_upload_photos(instance, file):
-    return f'photos/pereval-{instance.pereval.id}/{file}'
-
-# Сохранить фото в файл media, после добавления изображения, через админ панель.
